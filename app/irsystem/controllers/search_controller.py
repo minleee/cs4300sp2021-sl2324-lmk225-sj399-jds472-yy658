79edf940c56e30ca9d662b1cdf67b4c6ccd249c5
Grace Yim <yy658@cornell.edu>
2021-04-24 22:48:56 -0400

    update doc_norms

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -12,15 +12,11 @@ import app.irsystem.controllers.rank_mbti as m
 
 project_name = "Mo_Bie_TI"
 net_id = "Justin Sun (jds472), Lydia Kim (lmk225), Sohwi Jung (sj399), Seungmin Lee (sl2324), Grace Yim (yy658)"
-# inv_idx = np.load('data/inv_idx.npy',allow_pickle='TRUE').item()
-# idf = np.load('data/idf.npy',allow_pickle='TRUE').item()
-# doc_norms = np.loadtxt('data/doc_norms.txt', dtype=int)
-# mbti_dict = np.load('data/mbti_dict.npy',allow_pickle='TRUE').item()
-# inv_idx, idf, doc_norms, mbti_dict = m.precompute()
+
 inv_idx = json.load( open( "data/inv_idx.json" ) )
 idf = json.load( open( "data/idf.json" ) )
 character_dict = json.load( open( "data/character_dict.json" ) )
-doc_norms = np.loadtxt('data/doc_norms.txt', dtype=int)
+doc_norms = np.loadtxt('data/doc_norms.txt')
 movie_index = np.loadtxt('data/movie_index.txt', delimiter='\n', dtype=str, comments=None)
 updated_movie = np.loadtxt('data/updated_movie.txt')
 mbti_keys = ['INFJ', 'ENTP', 'INTP', 'INTJ', 'ENTJ', 'ENFJ', 'INFP', 'ENFP', 'ISFP', 'ISTP', 'ISFJ', 'ISTJ', 'ESTP', 'ESFP', 'ESTJ', 'ESFJ']
@@ -37,10 +33,6 @@ def search():
 		print('no query')
 		return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
 	else:
-		# inv_idx = json.load( open( "data/inv_idx.json" ) )
-		# idf = json.load( open( "data/idf.json" ) )
-		# doc_norms = np.loadtxt('data/doc_norms.txt', dtype=int)
-		# mbti_dict = json.load( open( "data/mbti_dict.json" ) )
 		output_message = "Your search: " + query
 		rankings = m.rank_mbtis(query, inv_idx, idf, doc_norms, mbti_keys)
 		movies = m.rank_movies(rankings, movie_index, updated_movie, mbti_keys)
